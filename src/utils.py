b29302ef473edf84ba922fdf9da188119f1c1922
Arvind Ram <arvindram03@gmail.com>
2016-10-08 17:29:16 -0400

    Revert "trying mob"
    This reverts commit a683c000223b5fd28faeb625ec0537632be00fe5.

--- src/utils.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-def get_logger(name, path):
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler(path, mode='w')
-    handler.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
-    return logger
