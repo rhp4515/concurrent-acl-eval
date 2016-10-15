c28bb58a246dbfa6b072f44eeb0f457f96b8915c
Hari Prasath Raman <hraman@cs.stonybrook.edu>
2016-10-15 12:05:11 -0400

    Making application read requests from xml file

--- src/policy.py
+++ src/policy.py
@@ -12,7 +12,7 @@ class PolicyParser():
         for rule in self.root.iter('rule'):
             sc = rule.find('subjectCondition')
             rc = rule.find('resourceCondition')
-            if sub['name'] == sc.attrib['name'] and res['name'] == rc.attrib['name']:
+            if sub['type'] == sc.attrib['type'] and res['type'] == rc.attrib['type']:
                 su = rule.find('subjectUpdate')
                 if su != None:
                     for attr in su.attrib:
@@ -25,7 +25,7 @@ class PolicyParser():
         for rule in self.root.iter('rule'):
             sc = rule.find('subjectCondition')
             rc = rule.find('resourceCondition')
-            if sub['name'] == sc.attrib['name'] and res['name'] == rc.attrib['name']:
+            if sub['type'] == sc.attrib['type'] and res['type'] == rc.attrib['type']:
                 ru = rule.find('resourceUpdate')
                 if ru != None:
                     for attr in ru.attrib:
@@ -33,84 +33,115 @@ class PolicyParser():
                             res_attrs.add(attr)
         return res_attrs
 
-    def check_sub_cond(self, sc, sub):
+    def resolve_expr(self, attr_value, sub, res):
+        value = None
+        if attr_value.find('$') == 0:
+            item, key = attr_value[1:].split('.')
+            if item == 'resource':
+                value = res[key]
+            elif item == 'subject':
+                value = sub[key]
+        return value
 
-        if sub['name'] != sc['name']:
+    def check_sub_cond(self, sc, sub, res):
+
+        if sub['type'] != sc['type']:
             return False
 
+        # print('subject condition', sc)
         for attr in sc:
             if attr in const.KEY_ATTRS:
                 continue
+
+            value = self.resolve_expr(sc[attr], sub, res)
+            if value is not None:
+                sc[attr] = value
+
             if sc[attr] == 'empty' and sub['attr'][attr] == '':
                 continue
+
+            if sc[attr] == '' and sub['attr'][attr] == '':
+                continue
+
             if sc[attr] != sub['attr'][attr]:
                 return False
 
         return True
 
-    def check_res_cond(self, rc, res):
-        if res['name'] != rc['name']:
+    def check_res_cond(self, rc, sub, res):
+        if res['type'] != rc['type']:
             return False
-
         for attr in rc:
             if attr in const.KEY_ATTRS:
                 continue
-            if rc[attr].find('<') > -1 and res['attr'][attr] >= int(rc[attr][1:]):
-                return False
-            elif rc[attr].find('>') > -1 and res['attr'][attr] <= int(rc[attr][1:]):
-                return False
-            elif rc[attr].find('=') > -1 and res['attr'][attr] != int(rc[attr][1:]):
-                return False
+            if rc[attr].find('<') > -1:
+                if res['attr'][attr] >= int(rc[attr][1:]):
+                    return False
+            elif rc[attr].find('>') > -1:
+                if res['attr'][attr] <= int(rc[attr][1:]):
+                    return False
+            else:
+                if res['attr'][attr] != int(rc[attr]):
+                    return False
 
         return True
 
-    def update_sub_attr(self, su, sub_attr):
+    def update_sub_attr(self, su, sub, res):
         # updating history
+        # print('updating subject attr')
+        sub_attr = sub['attr']
         for attr in su:
-            print('update_sub_attr', attr, sub_attr[attr], su[attr])
-            sub_attr[attr] = su[attr]
+            value = self.resolve_expr(su[attr], sub, res)
+            if value is None:
+                sub_attr[attr] = su[attr]
+            else:
+                sub_attr[attr] = value
         return sub_attr
 
-    def update_res_attr(self, ru, res_attr):
+    def update_res_attr(self, ru, sub, res):
         # updating view count
+        res_attr = res['attr']
         for attr in ru:
             if ru[attr] == '++':
                 res_attr[attr] += 1
             elif ru[attr] == '--':
                 res_attr[attr] -= 1
+            else:
+                res_attr[attr] = ru[attr]
         return res_attr
 
     def evaluate(self, sub, res, act):
         status = False
         # sub, res should contain some attrs from db
-        print('evaluate', sub, res, act)
+        # print('evaluate', sub, res, act)
         for rule in self.root.iter('rule'):
             sc=rule.find('subjectCondition')
-            if not self.check_sub_cond(sc.attrib, sub):
+            if not self.check_sub_cond(sc.attrib, sub, res):
                 continue
             rc=rule.find('resourceCondition')
-            if not self.check_res_cond(rc.attrib, res):
+            if not self.check_res_cond(rc.attrib, sub, res):
                 continue
 
             ac=rule.find('action')
-            if act['name'] != ac.attrib['name']:
+            if act['type'] != ac.attrib['type']:
                 continue
+            # All the conditions passed
+            status = True
+            # Do updates if there are any
             su=rule.find('subjectUpdate')
             if su != None:
                 # Updating attributes to the received request
-                sub['attr'] = self.update_sub_attr(su.attrib, sub['attr'])
-                status = True
+                sub['attr'] = self.update_sub_attr(su.attrib, sub, res)
 
             ru=rule.find('resourceUpdate')
             if ru != None:
-                res['attr'] = self.update_res_attr(ru.attrib, res['attr'])
-                status = True
+                res['attr'] = self.update_res_attr(ru.attrib, sub, res)
 
         return status, sub, res, act
 
     def parse(self):
         for rule in self.root.iter('rule'):
-            print('rule', rule.attrib['name'])
+            print('rule', rule.attrib['type'])
             sc=rule.find('subjectCondition')
             print('subject condition', sc.attrib)
             rc=rule.find('resourceCondition')
@@ -126,12 +157,12 @@ class PolicyParser():
             print()
 
 # p = PolicyParser()
-# sub = {'name':'customer', 'attr': {}, 'id':'2'}
-# res = {'name':'movie', 'attr': {'viewCount': 5}, 'id':'1' }
-# act = {'name': 'view'}
+# sub = {'type':'customer', 'attr': {}, 'id':'2'}
+# res = {'type':'song', 'attr': {'viewCount': 9}, 'id':'1' }
+# act = {'type': 'listen'}
 # print(p.evaluate(sub, res, act))
-# sub = {'name':'employee', 'attr': {'history': ''}, 'id': '3'}
-# res = {'name':'bank A', 'attr': {}}
-# act = {'name': 'read'}
-# # update res attrs only if the status is True
+# sub = {'type':'employee', 'attr': {'history': 'bank C'}}
+# res = {'type':'bank', 'id': 'bank B', 'attr': {}}
+# act = {'type': 'read'}
+# update res attrs only if the status is True
 # print(p.evaluate(sub, res, act))
