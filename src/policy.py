1be6e0a5de199beca5f83d58e92e05e283faed7c
Hari Prasath Raman <hraman@cs.stonybrook.edu>
2016-10-16 04:57:37 -0400

    3 test cases working

--- src/policy.py
+++ src/policy.py
@@ -35,12 +35,18 @@ class PolicyParser():
 
     def resolve_expr(self, attr_value, sub, res):
         value = None
+        print(attr_value)
         if attr_value.find('$') == 0:
             item, key = attr_value[1:].split('.')
+            print('resolve_expr sub', sub)
             if item == 'resource':
+                print('resolve_expr res', res)
+                print(item, key)
+                print(res[key])
                 value = res[key]
             elif item == 'subject':
-                value = sub[key]
+                return sub[key]
+        print ("value -- resolve_expr", value)
         return value
 
     def check_sub_cond(self, sc, sub, res):
@@ -52,8 +58,9 @@ class PolicyParser():
         for attr in sc:
             if attr in const.KEY_ATTRS:
                 continue
-
+            print(attr)
             value = self.resolve_expr(sc[attr], sub, res)
+            print ("value -- check_sub_cond", value)
             if value is not None:
                 sc[attr] = value
 
@@ -64,6 +71,9 @@ class PolicyParser():
                 continue
 
             if sc[attr] != sub['attr'][attr]:
+                print('sc attr', sc[attr])
+                print('sub attr', sub['attr'][attr])
+                print('Returning False')
                 return False
 
         return True
@@ -88,7 +98,7 @@ class PolicyParser():
 
     def update_sub_attr(self, su, sub, res):
         # updating history
-        # print('updating subject attr')
+        print('updating subject attr')
         sub_attr = sub['attr']
         for attr in su:
             value = self.resolve_expr(su[attr], sub, res)
@@ -100,6 +110,7 @@ class PolicyParser():
 
     def update_res_attr(self, ru, sub, res):
         # updating view count
+        print('updating resource attr')
         res_attr = res['attr']
         for attr in ru:
             if ru[attr] == '++':
@@ -126,17 +137,24 @@ class PolicyParser():
             if act['type'] != ac.attrib['type']:
                 continue
             # All the conditions passed
-            status = True
+
             # Do updates if there are any
             su=rule.find('subjectUpdate')
             if su != None:
                 # Updating attributes to the received request
                 sub['attr'] = self.update_sub_attr(su.attrib, sub, res)
+                status = True
+            else:
+                status = True
 
             ru=rule.find('resourceUpdate')
             if ru != None:
                 res['attr'] = self.update_res_attr(ru.attrib, sub, res)
-
+                status = True
+            else:
+                status = True
+            print('status', status)
+        print('status after all rules', status)
         return status, sub, res, act
 
     def parse(self):
@@ -161,7 +179,7 @@ class PolicyParser():
 # res = {'type':'song', 'attr': {'viewCount': 9}, 'id':'1' }
 # act = {'type': 'listen'}
 # print(p.evaluate(sub, res, act))
-# sub = {'type':'employee', 'attr': {'history': 'bank C'}}
+# sub = {'type':'employee', 'attr': {'history': 'bank B'}}
 # res = {'type':'bank', 'id': 'bank B', 'attr': {}}
 # act = {'type': 'read'}
 # update res attrs only if the status is True
